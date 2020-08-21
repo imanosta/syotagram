54396da077a134a398e070de93dfc994a086049b
konnoweram <konnoweram@gmail.com>
2020-08-21 09:37:06 +0900

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,10 +16,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-    public function boot()
-	{
-    	Schema::defaultStringLength(191);
-	}
 
     /**
      * Bootstrap any application services.
